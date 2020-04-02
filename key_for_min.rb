5d81a0b867434f3c22a0ed58916e60af76b6315d
Victor Plushko <vicplushko33@gmail.com>
2020-04-02 08:02:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash == 0
-    return nil
-end
+ min_key = 0
   name_hash.map do |key, value|
     
  end
